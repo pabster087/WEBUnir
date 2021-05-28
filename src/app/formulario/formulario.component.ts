25780463e845fd8dd71d22f759eaac77e23bf7be
Pablo <paayepezc@gmail.com>
2021-05-27 21:04:29 -0500

    Formulario plan

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-formulario',
@@ -6,24 +6,34 @@ import {Component, OnInit, ViewEncapsulation} from '@angular/core';
   styleUrls: ['./formulario.component.css'],
 })
 export class FormularioComponent implements OnInit {
-
+@ViewChild('mail', {static: true}) mail: ElementRef;
   interpolationBinding = 'Formulario de registro';
   propertyBinding = 'purple';
-  oculto = false;
+  oculto = true;
 
   newEmail = {
     email: '',
     title : 'Email',
   };
 
+  newNombre = {
+    name: '',
+    title : 'Nombre',
+    apellido: '',
+  };
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  // tslint:disable-next-line:typedef
   enviar($event){
     console.log($event);
     this.oculto = !this.oculto;
   }
-
+  enviarDatos(mail){
+    console.log(mail);
+    const userMail = this.mail.nativeElement.value;
+    console.log(userMail);
+  }
 }
